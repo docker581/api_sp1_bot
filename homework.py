ac9483c9458a2ac86fb5d0682d01a24dff2fd619
Denis Doktorov <denisd581@gmail.com>
2020-12-02 12:38:26 +0300

    Do homework

--- homework.py
+++ homework.py
@@ -7,43 +7,56 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+        'можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    params={'from_date':current_timestamp}
+    try:
+        homework_statuses = requests.get(
+            ('https://praktikum.yandex.ru/api/user_api/'
+            f'homework_statuses/'), 
+            headers=headers,
+            params=params,
+        )
+    except:
+        raise TypeError('Wrong response!')
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
-
+    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time()) 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    bot_client,
+                ),
+            current_timestamp = new_homework.get(
+                'current_date', 
+                current_timestamp,
+            )  
+            time.sleep(1200)  
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
---
+++ homework_sms_corrected.py
@@ -0,0 +1,54 @@
+import os
+import time
+import requests
+
+from twilio.rest import Client
+
+from dotenv import load_dotenv
+load_dotenv()
+
+URL = 'https://api.vk.com/method/'
+VK_VERSION = 5.92
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+NUMBER_TO = os.getenv('NUMBER_TO')
+ACCOUNT_SID = os.getenv('ACCOUNT_SID')
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+# определяем здесь, т.к. по pytest sms_sender() вызывается с 1 параметром
+CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+
+def get_status(user_id):
+    params = {
+        'user_ids': user_id,
+        'v': VK_VERSION,
+        'access_token': ACCESS_TOKEN,
+        'fields': 'online',
+    }
+    status = requests.post(
+        '{}users.get'.format(URL), 
+        params=params,
+    )
+    try:
+        status_online = status.json()['response'][0].get('online')
+    except:
+        raise TypeError('Wrong response!')
+    return status_online
+
+
+def sms_sender(sms_text):
+    message = CLIENT.messages.create(
+        body=sms_text,
+        from_=NUMBER_FROM,
+        to=NUMBER_TO,
+    )
+    return message.sid 
+
+
+if __name__ == '__main__':
+    vk_id = input('Введите id: ')
+    while True:
+        if get_status(vk_id) == 1:   
+            sms_sender(f'{vk_id} сейчас онлайн!')
+            break
+        time.sleep(5)
